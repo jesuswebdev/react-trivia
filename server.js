c2a5dafbd9ac8f9261a3aa34ae74261403d1fac2
Jesus Jimenez <jedar93@gmail.com>
2018-11-16 19:54:59 -0400

    modified start server script

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const path = require('path');
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+if (process.env.NODE_ENV === "development") {
+  require("./scripts/start");
+  return;
+}
+
+const express = require("express");
+const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(process.env.PORT || 8000, () => {console.log('server running at port 8000')});
+app.listen(process.env.PORT || 8000, () => {
+  console.log("server running at port 8000");
+});
