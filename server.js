5cc932adff9bea2cf239a3866710c4def22abde3
Jesus Jimenez <jedar93@gmail.com>
2018-11-19 21:33:57 -0400

    fix: robots.txt url not working

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
 app.get("/robots.txt", function(req, res) {
   res.type("text/plain");
   res.send("User-agent: *\nDisallow: /");
+});
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(process.env.PORT || 8000, () => {
