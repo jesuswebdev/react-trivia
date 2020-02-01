7298abbe35e9c1714c9332f4f38c6e210e696314
Jesus Jimenez <jesuswebdev@gmail.com>
2020-02-01 02:00:47 -0400

    fix header link and footer date

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -3,8 +3,8 @@ import { Layout } from "antd";
 
 const { Footer } = Layout;
 
-const AppFooter = props => {
-  return <Footer style={{ textAlign: "center" }}>React Trivia 2018</Footer>;
+const AppFooter = () => {
+  return <Footer style={{ textAlign: "center" }}>React Trivia 2020</Footer>;
 };
 
 export default AppFooter;
