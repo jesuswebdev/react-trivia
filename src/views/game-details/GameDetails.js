a60893d4c00dd86e20646e506acd4f5f11247521
Jesus Jimenez <jesuswebdev@gmail.com>
2020-02-01 17:53:29 -0400

    show loading skeleton when game details are being loaded

--- src/views/game-details/GameDetails.js
+++ src/views/game-details/GameDetails.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Row, Col, Card, Breadcrumb, Descriptions, List } from "antd";
+import { Row, Col, Card, Breadcrumb, Descriptions, List, Skeleton } from "antd";
 import DataProvider from "./provider";
 import {
   transformDate,
@@ -25,46 +25,52 @@ const GameDetails = props => {
               <Breadcrumb.Item>Detalles del juego</Breadcrumb.Item>
             </Breadcrumb>
             <Card>
-              <Descriptions title="Detalles del juego">
-                <Descriptions.Item label="Usuario">
-                  {game.user}
-                </Descriptions.Item>
-                <Descriptions.Item label="Fecha">
-                  {transformDate(game.created_at)}
-                </Descriptions.Item>
-                <Descriptions.Item label="Duración">
-                  {parseGameDuration(game.duration)}
-                </Descriptions.Item>
-                <Descriptions.Item label="Preguntas respondidas correctamente">
-                  {game.total_correct_answers}
-                </Descriptions.Item>
-              </Descriptions>
-              <List
-                itemLayout="vertical"
-                size="large"
-                header={<h3>Preguntas</h3>}
-                dataSource={game.questions}
-                renderItem={item => (
-                  <List.Item>
-                    <List.Item.Meta title={item.title} />
-                    <Descriptions>
-                      {item.answered && (
-                        <Descriptions.Item label="Respondida correctamente">
-                          {item.answered_correctly ? "Si" : "No"}
-                        </Descriptions.Item>
-                      )}
-                      {item.timed_out && (
-                        <Descriptions.Item label="Tiempo agotado">
-                          {item.timed_out ? "Si" : "No"}
-                        </Descriptions.Item>
-                      )}
-                      <Descriptions.Item label="Tiempo en responder">
-                        {`${parseQuestionDuration(item.duration)}s`}
-                      </Descriptions.Item>
-                    </Descriptions>
-                  </List.Item>
-                )}
-              />
+              {loading ? (
+                <Skeleton active />
+              ) : (
+                <>
+                  <Descriptions title="Detalles del juego">
+                    <Descriptions.Item label="Usuario">
+                      {game.user}
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Fecha">
+                      {transformDate(game.created_at)}
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Duración">
+                      {parseGameDuration(game.duration)}
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Preguntas respondidas correctamente">
+                      {game.total_correct_answers}
+                    </Descriptions.Item>
+                  </Descriptions>
+                  <List
+                    itemLayout="vertical"
+                    size="large"
+                    header={<h3>Preguntas</h3>}
+                    dataSource={game.questions}
+                    renderItem={item => (
+                      <List.Item>
+                        <List.Item.Meta title={item.title} />
+                        <Descriptions>
+                          {item.answered && (
+                            <Descriptions.Item label="Respondida correctamente">
+                              {item.answered_correctly ? "Si" : "No"}
+                            </Descriptions.Item>
+                          )}
+                          {item.timed_out && (
+                            <Descriptions.Item label="Tiempo agotado">
+                              {item.timed_out ? "Si" : "No"}
+                            </Descriptions.Item>
+                          )}
+                          <Descriptions.Item label="Tiempo en responder">
+                            {`${parseQuestionDuration(item.duration)}s`}
+                          </Descriptions.Item>
+                        </Descriptions>
+                      </List.Item>
+                    )}
+                  />
+                </>
+              )}
             </Card>
           </Col>
         </Row>
